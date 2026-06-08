616376f89dd6f97e82f857d7daaa05ed66c5f59c
Ryo <blue777sapphire@gmail.com>
2026-06-08 23:46:40 +0900

    add game loop

--- main.py
+++ main.py
@@ -4,26 +4,28 @@ answer = random.randint(100, 999)
 
 print("=== 3桁の数字当てゲーム ===")
 
-guess = int(input("数字を入力してください："))
+while True:
 
-if guess == answer:
-    print("正解！")
-else:
-    print("不正解")
+    guess = int(input("数字を入力してください："))
 
-    if guess < answer:
-        print("それより大きい数字です")
-
+    if guess == answer:
+        print("正解！")
     else:
-        print("それより小さい数字です")
+        print("不正解")
 
-    difference = abs(answer - guess)
+        if guess < answer:
+            print("それより大きい数字です")
 
-    if difference <= 10:
-        print("かなり近いです")
+        else:
+            print("それより小さい数字です")
 
-    elif difference <= 50:
-        print("近いです")
+        difference = abs(answer - guess)
 
-    else:
-        print("まだ遠いです")
+        if difference <= 10:
+            print("かなり近いです")
+
+        elif difference <= 50:
+            print("近いです")
+
+        else:
+            print("まだ遠いです")
